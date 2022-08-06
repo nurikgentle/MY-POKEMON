f8018efc152ecc77a125727fff8e8e2cd2459990
nurikgentle <nurikgentle@gmail.com>
2022-08-06 21:13:39 +0600

    its just a begining

--- src/components/FOOTER/Footer.jsx
+++ src/components/FOOTER/Footer.jsx
@@ -6,13 +6,15 @@ import Telegram from '../assets/Telegram.svg'
 import YouTube from '../assets/YouTube.svg'
 
 const Footer = () => {
+
+
   return (
     <div className='footer'>
       <h2>Подпишитесь на нас!</h2>
-      <img src={Instagram} alt='instagram'/>
-      <img src={Facebook} alt='facebook'/>
-      <img src={Telegram} alt='telegram'/>
-      <img src={YouTube} alt='YouTube'/>
+      <a href='https://youtu.be/y90yaLFoYoA'><img className='socials' src={Instagram} alt='instagram'/></a>
+      <a href='https://youtu.be/km728FNBInA'><img className='socials' src={Facebook} alt='facebook'/></a>
+      <a href='https://youtu.be/Hxab_Sr132o'><img className='socials' src={Telegram} alt='telegram'/></a>
+      <a href='https://youtu.be/XpKSIFGUQ9U'><img className='socials' src={YouTube} alt='YouTube'/></a>
     </div>
   )
 }
